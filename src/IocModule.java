b2f0a3a18133b2810cea03c1a46e3e1eed40f1eb
Camilo Sanchez <camilin87@gmail.com>
2018-06-07 21:02:16 -0400

    the smart worker depends in the workQueue. this should work but it doesn't

--- src/IocModule.java
+++ src/IocModule.java
@@ -3,6 +3,7 @@ import com.google.inject.AbstractModule;
 public class IocModule extends AbstractModule {
     @Override
     protected void configure() {
+        bind(IWorkQueue.class).to(RandomWorkQueue.class);
         bind(IWorker.class).to(SmartWorker.class);
     }
 }
--- src/SmartWorker.java
+++ src/SmartWorker.java
@@ -1,6 +1,12 @@
 public class SmartWorker implements IWorker {
+    private final IWorkQueue queue;
+
+    public SmartWorker(IWorkQueue queue) {
+        this.queue = queue;
+    }
+
     @Override
     public void DoWork() {
-        System.out.println("Working smarter");
+        System.out.println(String.format("Working smarter %d", this.queue.ReadWorkItem()));
     }
 }
